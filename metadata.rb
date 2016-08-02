209b79d72117e43eee0ebd63ece42be6754154a8
Eric Heydrick <eheydrick@gmail.com>
2016-08-01 17:43:43 -0700

    depend on newer php cookbook

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ supports            'ubuntu', '= 12.04'
 supports            'ubuntu', '= 14.04'
 
 depends             'apt',      '~> 2.6'
-depends             'php',      '~> 1.5'
+depends             'php',      '~> 1.10'
 depends             'php-fpm',  '~> 0.7'
 depends             'nginx',    '~> 2.7'
 depends             'mysql',    '~> 6.1'
